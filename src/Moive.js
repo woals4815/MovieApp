dcc4fb84c741ce652d75a37a9271625ff4e794f2
woals4815 <71315300+woals4815@users.noreply.github.com>
2020-11-04 22:03:26 +0900

    insert array of genres with list & complete CSS files

--- src/Moive.js
+++ src/Moive.js
@@ -2,14 +2,21 @@ import React from "react";
 import ProTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({ year, title, summary, poster}) {
+function Movie({ year, title, summary, poster, genres }) {
     return (
-        <div class="movie">
+        <div className="movie">
             <img src={poster} alt={title} title={title} />
-            <div class="movie__data">
-                <h3 class="movie__title">{title}</h3>
-                <h5 class="movie__year">{year}</h5>
-                <p class="movie__summary">{summary}</p>
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="movie__genres">
+                    {genres.map((genre, index) => (
+                        <li key={index} className="genres__genre">
+                            {genre}
+                        </li>
+                    ))}
+                </ul>
+                <p className="movie__summary">{summary}</p>                
             </div>
         </div>
     )
@@ -20,7 +27,8 @@ Movie.prototype = {
     year: ProTypes.number.isRequired,
     title: ProTypes.string.isRequired,
     summary: ProTypes.string.isRequired,
-    poster: ProTypes.string.isRequired
+    poster: ProTypes.string.isRequired,
+    genres: ProTypes.arrayOf(ProTypes.string).isRequired
 };
 
 export default Movie;
